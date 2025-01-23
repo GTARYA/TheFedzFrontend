511c2ab0cbd2f1193cd24df1c0e0cae6b854b2c8
sayedex <sayedadmod@gmail.com>
2025-01-23 18:09:47 +0600

    add custom button

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,7 +1,33 @@
 import { useAppKit } from '@reown/appkit/react'
+import { useEffect, useState } from 'react';
 
-export default function ConnectButton() {
+import { useAccount } from "wagmi";
+export default function Customconnetbtn() {
+  const [load,setLoad]  = useState(false);
     const { open, close } = useAppKit()
 
-    return <appkit-button />
-  }
+  const { address, isConnecting, isDisconnected } = useAccount();
+
+  const connetButton = () => {
+    // open({ view: 'Account' })
+    open();
+  };
+
+  useEffect(()=>{
+setLoad(true)
+  },[])
+  return (
+    <div className="flex justify-center items-center">
+    { load &&  <button
+        onClick={connetButton}
+        className=" py-[0px] h-[45px] rounded-2xl  bg-lightblue px-6 md:text-[18px] text-[16px] transition-all hover:hover:bg-[#7a2ed6] text-sm relative z-50 font-medium   text-white   "
+      >
+        <span className="mt-[1px]">
+          {address
+            ? address?.slice(0, 5) + "..." + address?.slice(-5)
+            : "Connect Wallet"}
+        </span>
+      </button>}
+    </div>
+  );
+}
