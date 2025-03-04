57867868888b94fba964b97af5830027d9c3d512
daniel assa <assa.daniel@gmail.com>
2025-03-04 13:17:40 +0200

    add slots access notifications to swap component

--- src/components/V4SwapComponent.tsx
+++ src/components/V4SwapComponent.tsx
@@ -32,6 +32,7 @@ import { poolId } from "../hooks/PoolConsts";
 import { balanceOf } from "../hooks/erc20";
 import { formatBalance } from "../hooks/formatters";
 import { on } from "events";
+import { isActingPlayer, isNftHolder } from "../hooks/fedz";
 
 
 let autofillTimeout: NodeJS.Timeout | undefined;
@@ -57,39 +58,18 @@ const V4SwapComponent = () => {
   const [isPlayerTurnState, setIsPlayerTurnState] = useState(false);
 
   useEffect(() => {
-    if (!mount && signer) {
+    if (!mount && signer && address) {
       fetchBalancesAndPrint();
       onAmountChange();
+      isNftHolder(address, signer).then((result: boolean) => {
+        setIsNFTHolderState(result);
+      });
+      isActingPlayer(address, signer).then((result: boolean) => {
+        setIsPlayerTurnState(result);
+      });
       setMount(true);
     }
-  }, [mount, signer]);
-
-  const { data: isNFTHolder } = useReadContract({
-    address: MockERC721Address,
-    abi: MockERC721Abi,
-    functionName: "isNFTHolder",
-    args: [address],
-  });
-
-  const { data: isPlayerTurn } = useReadContract({
-    address: TimeSlotSystemAddress,
-    abi: TimeSlotSystemAbi,
-    functionName: "canPlayerAct",
-    args: [address],
-  });
-
-  useEffect(() => {
-    if (isPlayerTurn !== undefined) {
-      setIsPlayerTurnState(isPlayerTurn as boolean);
-    }
-  }, [isPlayerTurn]);
-
-  useEffect(() => {
-    if (isNFTHolder !== undefined) {
-      setIsNFTHolderState(isNFTHolder as boolean);
-    }
-    console.log("is address NFT holder", isNFTHolder);
-  }, [isNFTHolder]);
+  }, [mount, signer, address]);
 
   const handleTokenSelection = (selectedToken: Token, isInput: boolean) => {
     if (isInput) {
@@ -225,24 +205,81 @@ const V4SwapComponent = () => {
                 />
               </div>
               <div className="pt-6">
-                <button
-                disabled={loading}
-                  onClick={arbSwap}
-                  className="btn btn-primary w-full hover:scale-105 transition-transform duration-200"
-                >
-                  {loading ? (
-                    <ScaleLoader
-                      height={20}
-                      loading={loading}
-                      color="#ffffff"
-                      className="text-white"
-                      aria-label="Loading Spinner"
-                      data-testid="loader"
-                    />
-                  ) : (
-                    "Swap"
-                  )}
-                </button>
+                {
+                  mount && isNFTHolderState && isPlayerTurnState && (
+                    <button
+                    disabled={loading}
+                      onClick={arbSwap}
+                      className="btn btn-primary w-full hover:scale-105 transition-transform duration-200"
+                    >
+                      {loading ? (
+                        <ScaleLoader
+                          height={20}
+                          loading={loading}
+                          color="#ffffff"
+                          className="text-white"
+                          aria-label="Loading Spinner"
+                          data-testid="loader"
+                        />
+                      ) : (
+                        "Swap"
+                      )}
+                    </button>
+                  )
+                }
+                {
+                  mount && (
+                    <>
+                      {
+                        isNFTHolderState ? (
+                          <>
+                            {
+                              !isPlayerTurnState && (
+                                <div className="alert alert-error">
+                                  <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="stroke-current shrink-0 h-6 w-6"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                  />
+                                </svg>
+                                <span>It is not your Turn to Act !</span>
+                              </div>
+                              )
+                            }
+
+                          </>
+                        )
+                        : (
+                            <div className="alert alert-warning">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="stroke-current shrink-0 h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                                />
+                              </svg>
+                              <span>
+                                You need to be an NFT Holder to add Liquidity !
+                              </span>
+                            </div>
+                        )
+                      }
+                    </>
+                  )
+                }
               </div>
             </div>
           </div>
