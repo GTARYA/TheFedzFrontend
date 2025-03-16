5aa2e7ed46b8c1c9041348d64e449413e6597583
daniel assa <assa.daniel@gmail.com>
2025-03-16 18:23:16 +0200

    set new timeslot system

--- src/contractAddressArbitrum.ts
+++ src/contractAddressArbitrum.ts
@@ -4,7 +4,7 @@ export const PoolSwapTestAddress = '0xA51afAFe0263b40EdaEf0Df8781eA9aa03E381a3';
 // real
 // export const PoolSwapTestAddress = '0xa857555633F521C3E0397c54a0CbF489011ff919';
 export const PoolModifyLiquidityTestAddress = '0xd88F38F930b7952f2DB2432Cb002E7abbF3dD869';
-export const HookAddress = 	'0x986522646bF8F57d3b28C5808edE9B1E5ccA0ac0';
+export const HookAddress = 	'0x88a8Cf314c5EBE9679bBC23ef6af8E0d7BCad3e0';
 export const MockUSDTAddress ='0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9';
 export const MockFUSDAddress = '0x894341be568Eae3697408c420f1d0AcFCE6E55f9';
 export const TimeSlotSystemAddress = '0x8b7373Ff3692B6a4aA9499BbACFacFd4C81A8724';
