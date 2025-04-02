e8c203e34831280a9fea61ebb5948cd2fe5f6817
sayedex <sayedadmod@gmail.com>
2025-04-02 23:27:58 +0600

    add subgraph

--- src/config/staking.ts
+++ src/config/staking.ts
@@ -4,4 +4,4 @@ export const STAKING_ADDR = "0x79b4F78615a7dec3C1Ca6Aa9318701051286eD51"
 export const LP_ADDR = "0xd88F38F930b7952f2DB2432Cb002E7abbF3dD869"
 export const FUSD_VAULT_ADDR = "0x0CE40C0Ee579CC98b294F6dE8b8B78805ab602Ec"
 export const TimeSlotSystemAddress = '0xbF378EfFB797Eeeab306506f8b03A3A7bfcBfe28';
-export const GRAPHQL_ENDPOINT = "https://api.studio.thegraph.com/query/44397/sbfusdstaking/version/latest"
+export const GRAPHQL_ENDPOINT = "https://api.studio.thegraph.com/query/44397/sbfusdstaking/0.0.1"
